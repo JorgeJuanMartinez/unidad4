c6f25c84595cc2280698559de8219dc17e2a4e2b
JorgeJuanMartinez <32667662+JorgeJuanMartinez@users.noreply.github.com>
2022-09-15 18:52:43 -0600

    Add files via upload

---
+++ PAWIDS/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Panel</title>
+	<link rel="stylesheet" type="text/css" href="public/css/main.css">
+</head>
+<body>
+
+	<div class="container">
+		<fieldset>
+
+			<form>
+			
+				
+				<legend>
+					Acceso al panel
+				</legend>
+
+				<label>
+					Correo Electronico
+				</label>
+				<input type="email" name="">
+
+				<label>
+					Contraseña
+				</label>
+				<input type="password" name="">
+
+				<button type="submit">
+					A C C E D E R
+				</button>
+			
+			</form>
+
+		</fieldset>
+
+	</div>
+
+</body>
+</html>
